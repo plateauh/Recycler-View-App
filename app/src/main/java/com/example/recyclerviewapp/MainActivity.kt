a2db60fd246f423f746fb7f12aa2f280662661eb
plateauh <70834431+plateauh@users.noreply.github.com>
2021-09-26 14:21:09 +0300

    no empty inputs

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.recyclerviewapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.EditText
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 lateinit var itemsRv: RecyclerView
 lateinit var items: ArrayList<String>
@@ -14,20 +16,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        items = arrayListOf<String>()
+        items = arrayListOf()
         val btn = findViewById<Button>(R.id.btn)
-        btn.setOnClickListener{
-            val input = findViewById<TextView>(R.id.etInput)
-            items.add(input.text.toString())
-            input.text = ""
+        val input = findViewById<EditText>(R.id.etInput)
+
+        btn.setOnClickListener {
+            addItem(input.text.toString())
+            input.setText("")
             updateRecyclerView()
         }
 
-
     }
 
-    fun updateRecyclerView() {
-        itemsRv = findViewById<RecyclerView>(R.id.rvItems)
+    private fun addItem(text: String){
+        if (text.isNotEmpty())
+            items.add(text)
+        else
+            Snackbar.make(cl, "Please enter something", Snackbar.LENGTH_LONG).show()
+    }
+
+    private fun updateRecyclerView() {
+        itemsRv = findViewById(R.id.rvItems)
         itemsRv.adapter = RecyclerViewAdapter(items)
         itemsRv.layoutManager = LinearLayoutManager(this)
     }
