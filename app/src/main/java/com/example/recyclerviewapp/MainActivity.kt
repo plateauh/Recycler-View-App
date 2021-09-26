5d6197bc3191160510889898108fdb2be983f26e
plateauh <70834431+plateauh@users.noreply.github.com>
2021-09-26 13:42:24 +0300

    Working but need a bit of UI enhancements

--- app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.recyclerviewapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+lateinit var itemsRv: RecyclerView
+lateinit var items: ArrayList<String>
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        items = arrayListOf<String>()
+        val btn = findViewById<Button>(R.id.btn)
+        btn.setOnClickListener{
+            val input = findViewById<TextView>(R.id.etInput)
+            items.add(input.text.toString())
+            input.text = ""
+            updateRecyclerView()
+        }
+
+
+    }
+
+    fun updateRecyclerView() {
+        itemsRv = findViewById<RecyclerView>(R.id.rvItems)
+        itemsRv.adapter = RecyclerViewAdapter(items)
+        itemsRv.layoutManager = LinearLayoutManager(this)
     }
 }
--- app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerviewapp/RecyclerViewAdapter.kt
@@ -1,4 +1,32 @@
 package com.example.recyclerviewapp
 
-class RecyclerViewAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_row.view.*
+
+class RecyclerViewAdapter (private val items: ArrayList<String>): RecyclerView.Adapter<RecyclerViewAdapter.ItemViewHolder>() {
+    class ItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        return ItemViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_row,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = items[position]
+        holder.itemView.apply {
+            tvItem.text = item
+        }
+    }
+
+    override fun getItemCount() = items.size
 }
